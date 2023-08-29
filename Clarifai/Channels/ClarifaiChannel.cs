7c6de45e00f087c75eb6a96535bcf5c4054620c5
Yvette Zhang <yvette@clarifai.com>
2023-08-28 23:02:01 -0400

    send max receive message length

--- Clarifai/Channels/ClarifaiChannel.cs
+++ Clarifai/Channels/ClarifaiChannel.cs
@@ -6,12 +6,14 @@ namespace Clarifai.Channels
     {
         public static Channel Grpc(string url = "api.clarifai.com")
         {
-            return new Channel(url, 443, new SslCredentials());
+            var maxReceiveMessageSizeOption = new ChannelOption(ChannelOptions.MaxReceiveMessageLength, 128 * 1024 * 1024); // 128 MB
+            return new Channel(url, 443, new SslCredentials(), new [] { maxReceiveMessageSizeOption });
         }
 
         public static Channel InsecureGrpc()
         {
-            return new Channel("api-grpc.clarifai.com", 18080, ChannelCredentials.Insecure);
+            var maxReceiveMessageSizeOption = new ChannelOption(ChannelOptions.MaxReceiveMessageLength, 128 * 1024 * 1024); // 128 MB
+            return new Channel("api-grpc.clarifai.com", 18080, ChannelCredentials.Insecure, new [] { maxReceiveMessageSizeOption });
         }
     }
 }
